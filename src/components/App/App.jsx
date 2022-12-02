62ebc49477241320b0239e34a8aa463cd405c1b6
Roman Mokrushin <emptyspaceman1@gmail.com>
2022-12-02 13:33:22 +0200

    fix bug with App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,3 @@
-import { Container } from './App.styled';
-
 import data from 'base/data.json';
 import friendsData from 'base/friends.json';
 import transactionsData from 'base/transactions.json';
@@ -9,6 +7,8 @@ import { Profile } from '../Profile/Profile';
 import { Statistics } from '../Statistics/Statistics';
 import { FriendList } from '../FriendList/FriendList';
 import { TransactionHistory } from '../TransactionHistory/TransactionHistory';
+
+import { Container } from './App.styled';
 
 export const App = () => {
   return (
