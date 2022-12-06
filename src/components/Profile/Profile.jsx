8dde72fa436f5efcc74be9f8ea4cfe85939ced2d
Roman Mokrushin <emptyspaceman1@gmail.com>
2022-12-06 13:23:43 +0200

    fixes markup according to Tech task

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -52,17 +52,17 @@ export function Profile({
   );
 }
 
-// Profile.propTypes = {
-//   username: PropTypes.string.isRequired,
-//   tag: PropTypes.string.isRequired,
-//   location: PropTypes.string.isRequired,
-//   avatar: PropTypes.string.isRequired,
-//   stats: PropTypes.exact({
-//     followers: PropTypes.number.isRequired,
-//     views: PropTypes.number.isRequired,
-//     likes: PropTypes.number.isRequired,
-//   }).isRequired,
-// };
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 /* <div className={css.profile}>
       <div className={css.description}>
